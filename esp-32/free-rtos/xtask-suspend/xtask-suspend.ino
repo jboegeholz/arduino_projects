b52e6c7e7e6ca5283f620532c74295f4bf669ef7
jboegeholz <boegeholz.joern@gmail.com>
2024-01-29 14:56:12 +0100

    rework

---
+++ esp-32/free-rtos/xtask-suspend/xtask-suspend.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+
+int count1 = 0;
+int count2 = 0;
+
+void task1(void* parameters){
+  for(;;){
+    Serial.print("Task 1 counter: ");
+    Serial.println(count1++);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    if(count1 == 3){
+      vTaskDelete(NULL);
+    }
+  }
+}
+
+void task2(void* parameters){
+  for(;;){
+    Serial.print("Task 2 counter: ");
+    Serial.println(count2++);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  xTaskCreate(
+    task1,      //function
+    "Task 1",   // function name
+    1000,       // stack size
+    NULL,       // paramters
+    1,          // priority
+    NULL        // handle
+  );
+  xTaskCreate(
+    task2,
+    "Task 2",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

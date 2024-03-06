373e2c6b2df54ded359f66ecb24be2a59febec33
jboegeholz <boegeholz.joern@gmail.com>
2024-03-06 22:07:30 +0100

    rework

--- arduino_uno/03_pwm/brightness-with-poti.ino
+++ arduino_uno/03_pwm/brightness-with-poti.ino
@@ -3,7 +3,7 @@ const int pwmPin = 9; // Use a PWM-capable pin
 void setup() {
   // Initialize the PWM pin as an output
   pinMode(pwmPin, OUTPUT);
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -11,7 +11,7 @@ void loop() {
   Serial.println("Wert vom Poti: " + String(sensorValue));
   int brightness = sensorValue/4;
 
-  analogWrite(9, brightness);
+  analogWrite(pwmPin, brightness);
 
 
 }

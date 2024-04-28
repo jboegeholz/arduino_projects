6470b328e7be40b9cc6d3b07cbc917724ed83f7a
jboegeholz <boegeholz.joern@gmail.com>
2024-04-28 13:10:21 +0200

    add traffic-light.ino

---
+++ arduino_uno/01_led_basics/display/display.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <SSD1306Ascii.h>
+#include <SSD1306AsciiWire.h>
+SSD1306AsciiWire oled;
+void setup()
+{
+  Wire.begin();
+  oled.setFont(System5x7);
+  oled.begin(&Adafruit128x64, 0x3C);
+}
+void loop()
+{
+  oled.println("Hello");
+  oled.print("www.funduino.de!");
+  delay(2000);
+  oled.clear();
+}
+
--- arduino_uno/01_led_basics/neopixel/neopixel.ino
+++ arduino_uno/01_led_basics/neopixel/neopixel.ino
@@ -3,33 +3,17 @@
 #define NUMPIXELS 12 
 Adafruit_NeoPixel modul_led_ring = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-
 void setup() {
   modul_led_ring.begin();
-
 }
 
-void loop() 
-{
-modul_led_ring.setPixelColor(0, modul_led_ring.Color(238,130,238));
-modul_led_ring.setPixelColor(1, modul_led_ring.Color(238,130,238));
-
-modul_led_ring.setPixelColor(2, modul_led_ring.Color(0,255,00));
-modul_led_ring.setPixelColor(3, modul_led_ring.Color(0,255,0));
-
-modul_led_ring.setPixelColor(4, modul_led_ring.Color(0,0,255));
-modul_led_ring.setPixelColor(5, modul_led_ring.Color(0,0,255));
-
-modul_led_ring.setPixelColor(6, modul_led_ring.Color(238,130,238));
-modul_led_ring.setPixelColor(7, modul_led_ring.Color(238,130,238));
-
-modul_led_ring.setPixelColor(8, modul_led_ring.Color(0,255,0));
-modul_led_ring.setPixelColor(9, modul_led_ring.Color(0,255,0));
-
-modul_led_ring.setPixelColor(10, modul_led_ring.Color(238,130,238));
-modul_led_ring.setPixelColor(11, modul_led_ring.Color(238,130,238));
-
-modul_led_ring.show();
+void loop() {
+  for(int i = 0; i < NUMPIXELS; i++) {
+    modul_led_ring.clear(); // Schaltet alle LEDs aus
+    modul_led_ring.setPixelColor(i, modul_led_ring.Color(255, 150, 0)); // Orangefarbene LEDs
+    modul_led_ring.show();
+    delay(500); // Warte 500ms zwischen den Änderungen
+  }
 }
 
 
